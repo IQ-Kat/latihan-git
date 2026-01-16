60c43bed048cb9be9b200311c618d67d7b639c1e
Ikbal Kat <ikbalkatili307@gmail.com>
2026-01-16 18:31:43 +0800

    Belajar Aritmatika Dasar // % ** serta and or not

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
-#Awal Pembelajaran Ikbal Tertanggal 16/01/2026
-"""Halo Ikbal di masa depan, ini awal kita melangkah, ingat kita masih kurang tahu, dan selamnya juga begitu jadi jangan berhenti belajar ya! Semangat terus!"""
-print ("Halo Dunia, ini adalah program Python pertamaku! Yah bukan pertama juga, tapi pertama di jalan serius ke AI Spesialis")
+skor_tes = 85
+pengalaman_coding_bulan = 0
+lulus_seleksi = skor_tes >= 70 and pengalaman_coding_bulan >= 6
+print (f"apakah ikbal lulus? {lulus_seleksi}")
 
-nama_dev = "Ikbal"
-umur_dev = 21
-tinggi_dev = 164.5
-tahun_lahir = 2026 - umur_dev
-apakah_pemula = True
-
-if apakah_pemula:
-    status_pemula = "Iya"
-else:
-    status_pemula = "Tidak"
-
-hasil = (f"Halo Perkenalkan Nama Saya {nama_dev}, Usia {umur_dev}, lahir pada tahun {tahun_lahir}, yah tinggi saya saat ini sekitar {tinggi_dev}, Saya Pemula? {status_pemula}")
-print (hasil)
+sisa_bagi = 10 % 3
+print (f"sisa bagi dari 10 dibagi 3 adalah {sisa_bagi}")
