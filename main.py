eee7dad27f823d774add8028ed49ee6f0ae3de43
Ikbal Kat <ikbalkatili307@gmail.com>
2026-01-16 18:16:49 +0800

    feat: print pertama

---
+++ main.py
@@ -0,0 +1,17 @@
+#Awal Pembelajaran Ikbal Tertanggal 16/01/2026
+"""Halo Ikbal di masa depan, ini awal kita melangkah, ingat kita masih kurang tahu, dan selamnya juga begitu jadi jangan berhenti belajar ya! Semangat terus!"""
+print ("Halo Dunia, ini adalah program Python pertamaku! Yah bukan pertama juga, tapi pertama di jalan serius ke AI Spesialis")
+
+nama_dev = "Ikbal"
+umur_dev = 21
+tinggi_dev = 164.5
+tahun_lahir = 2026 - umur_dev
+apakah_pemula = True
+
+if apakah_pemula:
+    status_pemula = "Iya"
+else:
+    status_pemula = "Tidak"
+
+hasil = (f"Halo Perkenalkan Nama Saya {nama_dev}, Usia {umur_dev}, lahir pada tahun {tahun_lahir}, yah tinggi saya saat ini sekitar {tinggi_dev}, Saya Pemula? {status_pemula}")
+print (hasil)
